e66c1f1f2912a7f2221a73a4d42f7dd3e0c5bac0
olschowsky <olschowsky@image-systems.biz>
2022-08-04 14:59:36 +0200

    rename

--- src/ui/wrapper.js
+++ src/ui/wrapper.js
@@ -2,58 +2,11 @@
 import Container from "../../lib/JST/dom/Container.js";
 
 
-const Wrapper = class {
-
-    /** @type {Container} */
-    #wrapper;
-
-    constructor(classNames) {
-        this.#wrapper = new Container(classNames);
-    }
-
-    /**
-     * @param {EventType} type
-     * @param {Function} listener
-     *
-     * @returns {Wrapper}
-     */
-    addEventListener(type, listener) {
-        this.#wrapper.addEventListener(type, listener);
-        return this;
-    }
-
-    addComponent(component) {
-        this.#wrapper.addComponent(component);
-        return this;
-    }
-
-    append(...components) {
-        this.#wrapper.append(components);
-        return this;
-    }
-
-    clear() {
-        this.#wrapper.clear();
-        return this;
-    }
-
-    getContainer() {
-        return this.#wrapper;
-    }
-
-    remove() {
-        this.#wrapper.remove();
-        return this;
-    }
+const Wrapper = class extends Container {
 
     /** @param {} renderer */
     render(renderer) {
-        renderer.addComponent(this.#wrapper);
-        return this;
-    }
-
-    replaceComponent(newComponent, oldComponent) {
-        this.#wrapper.replaceComponent(newComponent, oldComponent);
+        renderer.addComponent(this.getHTMLElement());
         return this;
     }
 
